5a2ca34dc062819e2877b6cce99d4627bb5fc995
Artem <artemzi1000@gmail.com>
2024-03-29 14:46:04 +0700

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
+import bridge from '@vkontakte/vk-bridge';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+bridge.send('VKWebAppInit');
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
